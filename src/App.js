67ef5d22b3c1a2cf724b5cac4283ef0445a68fa1
Dejmi89 <sticktothecode89@gmail.com>
2018-11-15 17:45:59 +0100

    mg first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        dada
+      </div>
+    );
+  }
+}
+
+export default App;
