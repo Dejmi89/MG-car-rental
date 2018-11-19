918448c089d58401feb997da627b2f8ecf8c57d7
Dejmi89 <sticktothecode89@gmail.com>
2018-11-19 19:48:01 +0100

    ss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-import form from './sass/_form.scss';
-
+import Form from './form';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
   render() {
     return (
-      <div className="right">
-        dada
+      <div >
+       <Form></Form>
+        {this.state.apiResponse}
       </div>
     );
   }
