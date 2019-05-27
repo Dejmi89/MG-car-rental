2c81ad850b2423ba2f351065e62b88b3a705cf2e
Dejmi89 <sticktothecode89@gmail.com>
2019-05-27 23:03:52 +0200

    proby

--- src/Offer_new.js
+++ src/Offer_new.js
@@ -1,30 +1,39 @@
 import React, { Component } from 'react';
 import styled, { keyframes } from 'styled-components';
 import img from './resources/images/wedding/0023_M&D_1080p.jpg';
-import img1 from './resources/images/wedding/mgPNG/1.png';
-import img2 from './resources/images/wedding/mgPNG/2.png';
-import img3 from './resources/images/wedding/mgPNG/3.png';
-import img4 from './resources/images/wedding/mgPNG/4.png';
-import img5 from './resources/images/wedding/mgPNG/5.png';
-import img6 from './resources/images/wedding/mgPNG/6.png';
-import img7 from './resources/images/wedding/mgPNG/7.png';
-import img8 from './resources/images/wedding/mgPNG/8.png';
-import img9 from './resources/images/wedding/mgPNG/9.png';
-import img10 from './resources/images/wedding/mgPNG/10.png';
-import img11 from './resources/images/wedding/mgPNG/11.png';
-import img12 from './resources/images/wedding/mgPNG/12.png';
-import img13 from './resources/images/wedding/mgPNG/13.png';
-import img14 from './resources/images/wedding/mgPNG/14.png';
+import img1 from './resources/images/wedding/mgPNG/photos/1_.png';
+import img2 from './resources/images/wedding/mgPNG/photos/2.png';
+import img3 from './resources/images/wedding/mgPNG/photos/3.png';
+import img4 from './resources/images/wedding/mgPNG/photos/4.png';
+import img5 from './resources/images/wedding/mgPNG/photos/5.png';
+import img6 from './resources/images/wedding/mgPNG/photos/6.png';
+import img7 from './resources/images/wedding/mgPNG/photos/7.png';
+import img8 from './resources/images/wedding/mgPNG/photos/8.png';
+import img9 from './resources/images/wedding/mgPNG/photos/9.png';
+import img10 from './resources/images/wedding/mgPNG/photos/10.png';
+import img11 from './resources/images/wedding/mgPNG/photos/11.png';
+import img12 from './resources/images/wedding/mgPNG/photos/12.png';
+import img13 from './resources/images/wedding/mgPNG/photos/13.png';
 
-const SlideIn = keyframes`
-    40%{
-        opacity:0;
-    }
+
+const flipImageY = keyframes`
     
     100% {
-        transform:translate(0,0);
+        transform: rotateY(360deg);
+        
+    }
+`;
+const flipImageX = keyframes`
+    
+    100% {
+        transform: rotateX(360deg);
         opacity: 1;
-
+    }
+`;
+const fadeIn = keyframes`
+    
+    100% {
+        opacity: 1;
     }
 `;
 
@@ -45,11 +54,15 @@ const FadeIn = styled.div`
 const PhotoGridContainer = styled.div`
     display:grid;
     grid-column: ${props => props.gridColumn};
-    grid-row: ${props => props.gridRow};;
+    grid-row: ${props => props.gridRow};
     background-image:url(${props => props.img});
     background-size:cover;
     background-position:center;
     margin:${props => props.margin};
+    opacity:0;
+    animation: ${props => props.animation} 1s forwards;
+    animation-delay: ${props => props.delay}s;
+   
 `;
 
 class OfferNew extends Component {
@@ -116,20 +129,20 @@ class OfferNew extends Component {
                     alignSelf:'center'                    
                 }}>
                     
-                    <PhotoGridContainer img={img1} gridColumn={'14/30'} gridRow={'4/30'} margin={'3%'}/>
-                    <PhotoGridContainer img={img2} gridColumn={'30/39'} gridRow={'3/15'} margin={'5%'}/>
-                    <PhotoGridContainer img={img3} gridColumn={'30/46'} gridRow={'15/24'} margin={'3%'}/>
-                    <PhotoGridContainer img={img4} gridColumn={'30/43'} gridRow={'24/31'} margin={'5%'}/>
-                    <PhotoGridContainer img={img5} gridColumn={'39/49'} gridRow={'1/15'} margin={'5%'}/>
-                    <PhotoGridContainer img={img6} gridColumn={'5/14'} gridRow={'12/35'} margin={'5%'}/>
-                    {/* <PhotoGridContainer img={img7} gridColumn={'1/5'} gridRow={'18/40'} margin={'5%'}/> */}
-                    <PhotoGridContainer img={img8} gridColumn={'9/14'} gridRow={'1/12'} margin={'5%'}/>
-                    <PhotoGridContainer img={img9} gridColumn={'43/46'} gridRow={'24/40'} margin={'5%'}/>
-                    <PhotoGridContainer img={img10} gridColumn={'46/49'} gridRow={'15/34'} margin={'3%'}/>
-                    <PhotoGridContainer img={img11} gridColumn={'49/54'} gridRow={'13/32'} margin={'5%'}/>
-                    <PhotoGridContainer img={img12} gridColumn={'49/54'} gridRow={'32/41'} margin={'7%'}/>
-                    <PhotoGridContainer img={img13} gridColumn={'18/26'} gridRow={'30/41'} margin={'5%'}/>
-                    <PhotoGridContainer img={img14} gridColumn={'38/43'} gridRow={'31/36'} margin={'5%'}/>
+                    <PhotoGridContainer img={img1} gridColumn={'12/31'} gridRow={'3/30'} margin={'4%'}  animation={fadeIn} delay={1.2}/>
+                    <PhotoGridContainer img={img2} gridColumn={'2/11'} gridRow={'11/41'} margin={'4%'}  animation={fadeIn} delay={2.1}/>
+                    <PhotoGridContainer img={img3} gridColumn={'12/31'} gridRow={'31/41'} margin={'4%'} animation={fadeIn} delay={1.4}/>
+                    <PhotoGridContainer img={img4} gridColumn={'32/42'} gridRow={'18/31'} margin={'4%'} animation={fadeIn} delay={1.6}/>
+                    <PhotoGridContainer img={img5} gridColumn={'32/49'} gridRow={'11/18'} margin={'4%'}  animation={fadeIn} delay={1.3}/>
+                    <PhotoGridContainer img={img6} gridColumn={'32/49'} gridRow={'3/11'}   margin={'4%'}  animation={fadeIn} delay={1.9}/>
+                    <PhotoGridContainer img={img7} gridColumn={'43/49'} gridRow={'18/37'} margin={'4%'} animation={fadeIn} delay={1.9}/>
+                    <PhotoGridContainer img={img8} gridColumn={'42/49'} gridRow={'38/41'} margin={'4%'}   animation={fadeIn} delay={1.3}/>
+                    <PhotoGridContainer img={img9} gridColumn={'12/49'} gridRow={'1/4'} margin={'4%'} animation={fadeIn} delay={1.4}/>
+                    <PhotoGridContainer img={img10} gridColumn={'49/52'} gridRow={'16/41'} margin={'4% 0 0 8%'} animation={fadeIn} delay={1.5}/>
+                    {/* <PhotoGridContainer img={img11} gridColumn={'52/54'} gridRow={'16/41'} margin={'3%'}animation={fadeIn} delay={1.6}/> */}
+                    <PhotoGridContainer img={img12} gridColumn={'49/54'} gridRow={'1/16'} margin={'4%'}animation={fadeIn} delay={1.7}/>
+                    <PhotoGridContainer img={img13} gridColumn={'4/11'} gridRow={'4/11'} margin={'4%'}animation={fadeIn} delay={1.8}/>
+                    
                     
 
                     
